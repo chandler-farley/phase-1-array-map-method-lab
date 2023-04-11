808d53dd98856af75c59c3cead54ded6e76398c9
Chandler Farley <chandlerfarley@live.com>
2023-04-10 20:37:54 -0500

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(str) {
+    return str.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+  })
 }
+
+/* function toTitleCase(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
+const titledTutorials = tutorials.map(toTitleCase);*/
